b7141e256e2c7ef4ef57978148ac1ba7a2b747e2
jwbaeck <jwbaeck97@gmail.com>
2024-07-05 14:28:15 +0900

    Setting: Update TailwindCSS and Vite configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        spin: "spin 1s linear infinite",
+      },
+      keyframes: {
+        spin: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
