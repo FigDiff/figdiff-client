26fadd6d67d270f66cc20def97510f5ee60ff450
정든결 <wantchurros@jeongdeungyeol-ui-MacBookAir.local>
2024-07-27 22:07:31 +0900

    Feat: Check user Id on Web Page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,12 @@ function App() {
   const { accessToken, setAccessToken } = useAuthStore();
 
   useEffect(() => {
+    chrome.storage.local.get("WEB_URL", (result) => {
+      if (!result.WEB_URL) {
+        chrome.storage.local.set({ WEB_URL: import.meta.env.VITE_WEB_URL });
+      }
+    });
+
     chrome.storage.local.get("data", (result) => {
       if (result.data && result.data.access_token) {
         setAccessToken(result.data.access_token);
