7fc83c0b7dd4acaeafb3e327c492f07663aef1d2
JungDeunGyul <ask7384@gmail.com>
2024-07-27 17:38:05 +0900

    Style: Change spinner to progress

--- src/components/Spinner.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Spinner: React.FC = () => {
-  return (
-    <div className="w-20 h-20 border-4 border-blue-500 border-dashed rounded-full animate-spin mb-4" />
-  );
-};
-
-export default Spinner;
