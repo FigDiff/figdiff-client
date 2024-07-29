7bbe71b47f973a3d3497800434ed007bdc0d62fe
JungDeunGyul <ask7384@gmail.com>
2024-07-30 06:58:38 +0900

    Refactor: Change variable names for clarity

--- public/renderDifferences.js
+++ public/renderDifferences.js
@@ -15,15 +15,18 @@ function renderDifferences(data) {
   const screenshotImage = document.createElement("img");
 
   screenshotImage.classList.add("screenshot-image");
-
   screenshotImage.src = screenshotUrl;
-
   screenshotImage.style.position = "absolute";
-  screenshotImage.style.left = "1.5px";
-  screenshotImage.style.top = "1.5px";
-  screenshotImage.style.border = "0px";
+  screenshotImage.style.left = "0px";
+  screenshotImage.style.top = "0px";
 
   document.body.appendChild(screenshotImage);
+
+  const diffContainer = document.createElement("div");
+  diffContainer.classList.add("diff-Container");
+  diffContainer.style.position = "absolute";
+  diffContainer.style.left = "0px";
+  diffContainer.style.top = "0px";
 
   data.differentFigmaNodes.forEach((node, index) => {
     const { x, y, width, height } = node.absoluteBoundingBox;
@@ -31,26 +34,22 @@ function renderDifferences(data) {
     const blob = new Blob([imageBuffer], { type: "image/png" });
     const imageUrl = URL.createObjectURL(blob);
 
-    const diffContainer = document.createElement("div");
-
-    diffContainer.classList.add("figma-image");
-
-    diffContainer.style.position = "absolute";
-    diffContainer.style.left = `${x}px`;
-    diffContainer.style.top = `${y}px`;
-    diffContainer.style.width = `${width}px`;
-    diffContainer.style.height = `${height}px`;
-    diffContainer.style.zIndex = "9999";
-    diffContainer.style.pointerEvents = "none";
-    diffContainer.style.border = "2px solid green";
-    diffContainer.style.opacity = "0.2";
-
     const diffImage = document.createElement("img");
+    diffImage.classList.add("figma-image");
+    diffImage.style.position = "absolute";
+    diffImage.style.left = `${x}px`;
+    diffImage.style.top = `${y}px`;
+    diffImage.style.width = `${width}px`;
+    diffImage.style.height = `${height}px`;
+    diffImage.style.zIndex = "9999";
+    diffImage.style.pointerEvents = "none";
+    diffImage.style.border = "2px solid green";
     diffImage.src = imageUrl;
 
     diffContainer.appendChild(diffImage);
-    document.body.appendChild(diffContainer);
   });
+
+  document.body.appendChild(diffContainer);
 
   createCombinedOpacityControl();
 }
@@ -124,9 +123,7 @@ function createCombinedOpacityControl() {
 
   figmaSlider.addEventListener("input", () => {
     const opacityValue = figmaSlider.value / 100;
-    document.querySelectorAll(".figma-image").forEach((element) => {
-      element.style.opacity = opacityValue;
-    });
+    document.querySelector(".diff-Container").style.opacity = opacityValue;
   });
 
   webpageSlider.addEventListener("mousedown", (e) => {
